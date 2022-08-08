ddd52f39981c589d60e294039619a212b0db71b1
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-08 16:02:05 -0400

    post model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const Post = require('./Posts');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
 
 module.exports = { User }
