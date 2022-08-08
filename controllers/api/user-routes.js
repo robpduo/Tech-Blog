a429f5781c4cd715e08e1d7677fef0081a972999
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-08 12:29:35 -0400

    added session in user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -33,6 +33,7 @@ router.get('/:id', (req, res) => {
         });
 });
 
+// create a user, start a new session on the user's id
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
@@ -40,12 +41,18 @@ router.post('/', (req, res) => {
         password: req.body.password
     })
         .then(dbUserData => {
-            res.json(dbUserData);
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
-        })
+        });
 });
 
 
@@ -106,9 +113,15 @@ router.post('/login', (req, res) => {
                 res.status(400).json({ message: 'Incorrect Password!' });
                 return;
             }
-            
-            res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json({ user: dbUserData, message: 'You are now logged in!' });
+            });
         });
-})
+});
 
 module.exports = router;
