dd8c5488607b5fd18d3da8bc9e6074af77c3bd2b
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-09 12:44:28 -0400

    render all posts on homepage, render login and signup form

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
+const CommentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', CommentRoutes);
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,43 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('login', {
+    console.log('======================');
+    Post.findAll({
+        attributes: [
+            'id',
+            'blog_content',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
 
-    });
+            res.render('allPosts', {
+                posts
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
