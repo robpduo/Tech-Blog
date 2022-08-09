ef86f649bc6e5733a9c5fe5940ea4c9daabd7940
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-09 11:15:39 -0400

    comment model

--- models/User.js
+++ models/User.js
@@ -57,15 +57,4 @@ User.init(
     }
 );
 
-
-router.post('/logout', (req, res) => {
-    if(req.session.loggedIn) {
-        req.session.destroy(() => {
-            res.status(204).end();
-        });
-    } else {
-        res.status(404).end();
-    }
-});
-
 module.exports = User; 
