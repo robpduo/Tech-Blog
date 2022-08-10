272938096169d84b651d68f785676c3b63c45224
robpduo <101683611+robpduo@users.noreply.github.com>
2022-08-10 16:40:49 -0400

    signup button on a separate page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -22,6 +22,8 @@ async function loginFormHandler(event) {
         } else {
             alert(response.statusText);
         }
+    } else {
+        alert('All fields must be filled in');
     }
 }
 
